3d0847913e0a77905d2db70d285a32c35ee63623
aditya <you@example.com>
2024-06-28 16:22:33 +0530

    fixed shadow acne

--- camera.h
+++ camera.h
@@ -78,7 +78,7 @@ class camera {
 				return color(0,0,0);
 			hit_record rec;
 
-			if(world.hit(r, interval(0, infinity), rec)){
+			if(world.hit(r, interval(1e-3, infinity), rec)){
 				vec3 direction = random_on_hemisphere(rec.normal);
 				return 0.7*ray_color(ray(rec.p, direction), world, depth - 1);
 			}
