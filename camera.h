00ca2ea062bfad872fe79d7acb26aa2be8588ab3
aditya <you@example.com>
2024-06-28 16:26:35 +0530

    upgrade random diffusion to lambertian diffusion

--- camera.h
+++ camera.h
@@ -79,7 +79,7 @@ class camera {
 			hit_record rec;
 
 			if(world.hit(r, interval(1e-3, infinity), rec)){
-				vec3 direction = random_on_hemisphere(rec.normal);
+				vec3 direction = rec.normal + random_unit_vector();
 				return 0.7*ray_color(ray(rec.p, direction), world, depth - 1);
 			}
 
