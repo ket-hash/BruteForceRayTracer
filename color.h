a8eea8889564f109242ec0c3ff7f4a3a44d99909
aditya <you@example.com>
2024-06-28 16:35:07 +0530

    added gamma transform

--- color.h
+++ color.h
@@ -6,10 +6,21 @@
 
 using color = vec3;
 
+inline float lin_to_gamma(float linear) {
+	if (linear >= 0.0){
+		return sqrt(linear);
+	}
+	return 0;
+}
+
 void write_color(std::ostream& out, const color& pixels){
 	auto r = pixels.x();
 	auto g = pixels.y();
 	auto b = pixels.z();
+
+	r = lin_to_gamma(r);
+	g = lin_to_gamma(g);
+	b = lin_to_gamma(b);
 	
 	static const interval intensity(0.000, 0.999);
 	int rbyte = int(256*intensity.clamp(r));
